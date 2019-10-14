512935991249ad561b225ddd7a117c2217c3e65d
dvalerio <dnotms290>
2019-10-14 16:08:48 -0300

    Remoção da inicialização e dependencias do slider que deu erro

--- app/src/main/java/com/danilovalerio/mycoin/MainActivity.kt
+++ app/src/main/java/com/danilovalerio/mycoin/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.danilovalerio.mycoin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.heinrichreimersoftware.materialintro.app.IntroActivity
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : IntroActivity() {
+
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
