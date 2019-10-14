4e90de1a0035858d61f00115ecc81a1fb3cdb6c0
dvalerio <dnotms290>
2019-10-14 17:40:59 -0300

    Criação da tela de cadastro e da tela de login

--- app/src/main/java/com/danilovalerio/mycoin/MainActivity.kt
+++ app/src/main/java/com/danilovalerio/mycoin/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.danilovalerio.mycoin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.danilovalerio.mycoin.activity.CadastroActivity
+import com.danilovalerio.mycoin.activity.LoginActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        listeners()
 
+    }
+
+    private fun listeners(){
+        btnCadastrar.setOnClickListener(){
+            startActivity(Intent(this,CadastroActivity::class.java))
+        }
+
+        tvJaTenhoConta.setOnClickListener(){
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
