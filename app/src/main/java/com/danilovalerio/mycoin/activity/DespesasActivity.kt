bf4466087f9df7fc6dc51ff6af3960efd967170c
dvalerio <dnotms290>
2019-10-15 22:14:52 -0300

    Telas Receitas e Despesas

--- app/src/main/java/com/danilovalerio/mycoin/activity/DespesasActivity.kt
+++ app/src/main/java/com/danilovalerio/mycoin/activity/DespesasActivity.kt
@@ -3,11 +3,14 @@ package com.danilovalerio.mycoin.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.danilovalerio.mycoin.R
+import kotlinx.android.synthetic.main.activity_principal.*
 
 class DespesasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_despesas)
+
+
     }
 }
